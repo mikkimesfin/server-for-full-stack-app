18585fb1038c419bcd427211bfaabbb6c8a30023
Mikki Mesfin <mikki.mesfi@gmail.com>
2017-07-26 15:04:47 -0600

    post request

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express')
 var server = express()
 var cors = require('cors')
 var knex = require('./db/knex')
+var bodyParser = require('body-parser')
 
 server.use(cors());
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended:false}))
 
 server.get('/', function(req, res) {
   knex('inventory').then((inventory) => {
@@ -19,5 +23,12 @@ server.get('/:id', function(req, res) {
   })
 })
 
-
+server.post('/', function(req, res) {
+  knex('inventory').insert(req.body)
+  .returning('id')
+  .then((inventory) => {
+    res.json(inventory)
+    console.log(inventory)
+  })
+})
 server.listen(process.env.PORT || 8080)
